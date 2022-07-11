983e41dc3fe8a9c3fabb689b4e5378e5dcea565d
Jonathan <mrjonathangalloway@yahoo.com>
2022-07-11 18:55:08 -0400

    counting

---
+++ script.js
@@ -0,0 +1,15 @@
+//for loop from 1 to 10
+for (let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+//from 10 to 0
+for (let i = 10; i >= 0; i--){
+    console.log(i);
+}
+while (i<=10){
+  (i);
+  i++;
+}
+
+let number = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
